592f47924d1d1058f98e8e0dcc74968136681f9d
tsxepo <tsxepo@gmail.com>
2022-11-26 15:15:43 +0200

    Revert "adding a reference"
    This reverts commit 9e5e08d191424248bea66072d8f7129af3b2f386.

---
+++ TourOfHeroesBackend/Repositories/HeroRepository.cs
@@ -0,0 +1,47 @@
+﻿using TourOfHeroesBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace TourOfHeroesBackend.Repositories;
+
+public class HeroRepository: IHeroRepository
+{
+    private readonly HeroContext _context;
+    public HeroRepository(HeroContext context)
+    {
+        _context= context;
+    }
+    public async Task<Hero> Create(Hero hero)
+    {
+        _context.Heroes.Add(hero);
+        await _context.SaveChangesAsync();
+        return hero;
+    }
+
+    public async Task Delete(int Id)
+    {
+        var heroToDelete = await _context.Heroes.FindAsync(Id);
+        _context.Heroes.Remove(heroToDelete);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Hero>> Get()
+    {
+        return await _context.Heroes.ToListAsync();
+    }
+
+    public async Task<Hero> Get(int Id)
+    {
+        return await _context.Heroes.FindAsync(Id);
+    }
+
+    public async Task Update(Hero hero)
+    {
+        _context.Entry(hero).State = EntityState.Modified;
+        await _context.SaveChangesAsync();  
+    }
+}
