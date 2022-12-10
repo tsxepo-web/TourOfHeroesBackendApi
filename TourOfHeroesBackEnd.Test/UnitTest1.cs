0968f0e00efe184da0e73ce55ca1e00d1d8c09b6
tsxepo <tsxepo@gmail.com>
2022-12-10 20:32:13 +0200

    initialise test

---
+++ TourOfHeroesBackEnd.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TourOfHeroesBackEnd.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
