7b25def888333347695c291b0d9a8bbc1a2e5c01
tsxepo <tsxepo@gmail.com>
2023-01-19 02:56:07 +0200

    added new migrations

---
+++ HeroesDB/Migrations/20230119003159_ModifyProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeroesSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Powers = table.Column<int>(type: "int", nullable: false),
+                    Weatherboost = table.Column<int>(type: "int", nullable: false),
+                    PowerLevel = table.Column<int>(type: "int", nullable: false),
+                    IsVillain = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heroes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Heroes");
+        }
+    }
+}
