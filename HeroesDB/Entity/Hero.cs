91ba0a1391ade75405b8cc98c4f08bc48e269a99
tsxepo <tsxepo@gmail.com>
2023-01-19 02:31:41 +0200

    change int array to int

--- HeroesDB/Entity/Hero.cs
+++ HeroesDB/Entity/Hero.cs
@@ -5,7 +5,7 @@ public class Hero
     public int Id { get; set; } 
     public string? Name { get; set; }
     public Powers Powers { get; set; }
-    public int[]? Weatherboost { get; set; }
+    public int Weatherboost { get; set; }
     public int PowerLevel {get; set;}
     public bool IsVillain {get; set;}
 }
