bc20b9592f1a30508117bc96d43663a63c9632a1
tsxepo <tsxepo@gmail.com>
2023-02-05 17:33:37 +0200

    set origins

--- TourOfHeroesBackend/Program.cs
+++ TourOfHeroesBackend/Program.cs
@@ -56,7 +56,7 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseCors("*");
+app.UseCors("MyAllowedSpecificOrigins");
 app.MapControllers();
 
 app.Run();
