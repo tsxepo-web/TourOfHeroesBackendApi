c22c6f18478fd943bce084993a5fb91b25e233b6
tsxepo <tsxepo@gmail.com>
2023-02-05 17:22:43 +0200

    set cors to all

--- TourOfHeroesBackend/Program.cs
+++ TourOfHeroesBackend/Program.cs
@@ -56,7 +56,7 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseCors("MyAllowedSpecificOrigins");
+app.UseCors("*");
 app.MapControllers();
 
 app.Run();
